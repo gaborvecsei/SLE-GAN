fc1acc885a58ca16a814b025502b2ae4b149ad21
Robert Ronan <37887237+rlronan@users.noreply.github.com>
2021-05-29 15:44:55 -0400

    Bug fixes, many improvements, and updates to match offical repository
    ## Fixes:
    - Moved model weight loading into the training loop in train.py, so it works properly. The model needs to be initialized for the weights to be loaded.
    - Fixed code for Discriminator weight loading. 
    - Divided DownSamplingBlock output by two so that it is an average of the two sub-outputs, rather than a sum.
    
    ## Updates:
    - Added Tensorboard metric logging for every step, rather than just every epoch.
    - Added Visualizations at every 500 steps if the epoch is longer than 500 steps.
    - Changed image resizing in training_routine.py to use 'nearest' method since the resizing is by a factor of 1/2. 
    - create_dataset function now also tries to find .png files if no .jpg files are found. 
    - center_crop_image function now performs random crops, but is still called 'center_crop_images'
    - Replaced BatchNormalization with LayerNormalization everywhere, as it is more appropriate to use with small batch sizes. 
    
    ## Updates to match official implementation
    - Replaced reconstruction loss MAE function with perception loss as per the official repository implementation, and because MAE is not a good image distance metric.  
    - Included a forked version of https://github.com/alexlee-gk/lpips-tensorflow  which i updated for TF2.x to perform the perception loss.
    - In the discriminator, added a spectral norm wrapper to every conv layer, and updated leakyrelu rate to 0.2
    - In the discriminator, added SLE blocks as per the official repository; this should improve the gradient flow in the discriminator. 
    - In the generator, included the code for upsampling blocks that match the official repository (under UpsamplingBlock2). These have a small amount of Gaussian noise added, and are twice as many layers as the current UpsamplingBlock. In the official repository, both types of upsampling blocks are interspersed.  
    - Turned bias off for every conv layer in the discriminator and generator.
    - Replaced leakyrelu with silu (aka swift) activation function in SLE blocks.

--- sle_gan/losses.py
+++ sle_gan/losses.py
@@ -1,8 +1,13 @@
 import tensorflow as tf
-
+from .lpips_tf import lpips
 
 def discriminator_reconstruction_loss(real_image, decoded_image):
-    return tf.reduce_mean(tf.keras.losses.MAE(real_image, decoded_image))
+    # replaced MAE loss with perception loss from official implementation. 
+
+    # expects images in [0,1] 
+    distance_percept = lpips((real_image + 1)/2, (decoded_image + 1)/2, model='net-lin', net='vgg')
+    return tf.reduce_mean(distance_percept)
+
 
 
 def discriminator_real_fake_loss(real_fake_output_logits_on_real_images, real_fake_output_logits_on_fake_images):
